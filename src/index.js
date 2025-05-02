23507a23b0dad8b336f5503655ac2bebb4641b42
Svetlana <brigantina@mail.ru>
2025-05-02 19:44:52 +0300

    Update index.js
    fix function

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 module.exports = function reverse(num) {
   throw new Error('Not implemented');
   let result = 0;
+  while (num) {
+    result = result + (num % 10);
+    num = Math.floor(num);
+  }
+  return result;
 };
+reverse()
